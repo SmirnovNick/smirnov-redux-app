775542f2d70a4df9989f9f482fd02a33501cc6ec
Smirnov Nick <madniks@mail.ru>
2017-07-31 11:47:28 +0300

    Кривое дерево тоже дерево)

--- src/GetTreeNEW.js
+++ src/GetTreeNEW.js
@@ -1,33 +1,37 @@
+import getTree from './getTreeOLD';
+
 
 
 export default function GetTree(array, parentsID = []) {
+  var tqwerty = getTree(array);
   var parents = {};
 
   for (var i = 0; i < array.length; i++) {
     if (!parentsID.includes(array[i]['parent'])){
       parentsID.push(array[i]['parent']);
+      let rrrr = parseInt(array[i]['id']);
       Object.defineProperty(parents, array[i]['parent'], {value :  [{
         id:array[i]['id'],
         title:array[i]['title'],
         level: 0,
-        child: parents[parseInt(array[i]['id'])]
+        child: parents[array[i]['id']]
       }]});
     } else {
         parents[array[i]['parent']].push({
           id:parseInt(array[i]['id']),
           title:array[i]['title'],
           level: 0,
-          child: parents[parseInt(array[i]['id'])]
+          child: parents[array[i]['id']]
         });
 
 
   }
-
+var output = parents[null];
 
 
 }
 console.log('------ID Родителей------');
 
-console.log('--------Родители--------');
-console.log(parents);
+console.log(tqwerty);
+console.log(output);
 }
