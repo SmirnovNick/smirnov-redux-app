0f0b3d82d031e172ec2e5cd46c703bc455304d0a
Smirnov Nick <madniks@mail.ru>
2017-07-31 11:22:49 +0300

    GetTreeNEW 50%

--- src/GetTreeNEW.js
+++ src/GetTreeNEW.js
@@ -4,19 +4,30 @@ export default function GetTree(array, parentsID = []) {
   var parents = {};
 
   for (var i = 0; i < array.length; i++) {
-
-    if (!Object.keys(parents).includes(array[i]['parent'])) {
-      Object.defineProperty(parents, array[i]['parent'], {value : array[i]['id']})
+    if (!parentsID.includes(array[i]['parent'])){
+      parentsID.push(array[i]['parent']);
+      Object.defineProperty(parents, array[i]['parent'], {value :  [{
+        id:array[i]['id'],
+        title:array[i]['title'],
+        level: 0,
+        child: parents[parseInt(array[i]['id'])]
+      }]});
     } else {
+        parents[array[i]['parent']].push({
+          id:parseInt(array[i]['id']),
+          title:array[i]['title'],
+          level: 0,
+          child: parents[parseInt(array[i]['id'])]
+        });
 
-
-    }
 
   }
 
-  console.log('------ID Родителей------');
 
-  console.log('--------Родители--------');
-  console.log(parents);
 
+}
+console.log('------ID Родителей------');
+
+console.log('--------Родители--------');
+console.log(parents);
 }
